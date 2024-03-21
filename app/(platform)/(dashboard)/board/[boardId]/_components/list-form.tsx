fe0cf75f4c4c5986184183a3248eb8473f81446b
Kgatliso Matema <kgatliso481@live.com>
2024-03-21 15:44:35 +0200

    taskify

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -0,0 +1,111 @@
+"use client"
+
+import React, {useState, useRef, ElementRef} from 'react'
+import ListWrapper from './list-wrapper'
+import { Plus, X } from 'lucide-react'
+import { useEventListener, useOnClickOutside } from 'usehooks-ts';
+
+import { useAction } from '@/hooks/use-action';
+import { createList } from '@/actions/create-list';
+import { FormInput } from '@/components/form/form-input';
+import { useParams, useRouter } from 'next/navigation';
+import { FormSubmit } from '@/components/form/form-submit';
+import { toast } from 'sonner';
+
+
+export default function ListForm() {
+  const router = useRouter();
+  const params = useParams();
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const {execute, fieldErrors} = useAction(createList, {
+     onSuccess: (data) => {
+       toast.success(`List "${data.title}" created.`, {
+         position: 'top-center'
+       });
+       disableEditing();
+       router.refresh();
+     },
+     onError: (error) => {
+        toast.error(error, {
+            position: 'top-center'
+        });  
+     }
+  })
+
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+       inputRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+  
+   const onKeyDown = (e: KeyboardEvent) => {
+     if(e.key === "Escape"){
+        disableEditing();
+     };
+   };
+
+
+   useEventListener("keydown", onKeyDown);
+   useOnClickOutside(formRef, disableEditing);
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = formData.get("boardId") as string;
+
+    execute ({
+        title,
+        boardId
+    });
+  }
+
+
+
+  if(isEditing) {
+     return (
+        <ListWrapper>
+           <form
+             action={onSubmit}
+             ref={formRef}
+             className='w-full p-3 rounded-md bg-white space-y-4 shadow-md'>
+             <input hidden value={params.boardId} name='boardId'/>
+             <FormInput 
+                ref={inputRef}
+                errors={fieldErrors}
+                id='title'
+                className='text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition'
+                placeholder='Enter list title...'
+              />
+              <div className='flex items-center gap-x-1'>
+                  <FormSubmit>
+                     Add list
+                  </FormSubmit>
+              </div>
+            </form>
+        </ListWrapper>
+     )
+  }
+
+
+  return (
+    <ListWrapper>
+           <button 
+             onClick={enableEditing}
+             className='w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm'>
+            <Plus className='h-4 w-4 mr-2'/>
+              Add a list
+           </button>
+     
+    </ListWrapper>
+  )
+}
